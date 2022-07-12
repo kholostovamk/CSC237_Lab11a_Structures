71e0db3afd93ce75ccd31a26358ceb5b8cbf51fb
margo.khm <kholostova@gmail.com>
2022-07-12 08:43:05 -0500

    created dynamically allocated array

--- lab11a.cpp
+++ lab11a.cpp
@@ -7,5 +7,15 @@ struct movieData
     string title; //movie title
     string director; //movie director
     int yearReleased; //year Released
-    double runningTime; //Running time in minutes
+    double runningTime; //Running time in minu
 };
+
+int main() {
+
+    int array_size;
+    double *arrayPtr;
+    cout << "Enter desired array size: ";
+    cin >> array_size;
+    arrayPtr = new double[array_size];
+    
+}
