bef5ff9482eef47741d4e0e7c6c8218d50dd8ca9
margo.khm <kholostova@gmail.com>
2022-07-12 08:34:52 -0500

    struct created

---
+++ lab11a.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct movieData
+{
+    string title; //movie title
+    string director; //movie director
+    int yearReleased; //year Released
+    double runningTime; //Running time in minutes
+};
